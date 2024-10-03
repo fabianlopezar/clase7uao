672459d16050e62f5ce39cd42b619395faf3dbea
David Roa <fabianlopez928@gmail.com>
2024-10-03 18:09:08 -0500

    Initial commit

---
+++ Basics/string.js
@@ -0,0 +1,12 @@
+let string1 = "hola mundo 2";
+let string2 = "hola mundo 3";
+let string3 = `${string1},${string2}, hola mundo 4`;
+
+console.log(string3);
+
+console.log(string3.length);
+console.log(string3.toLocaleUpperCase());
+console.log(string3.toLocaleLowerCase());
+
+let frase = "hola mundo 5!";
+console.log(frase.substring(0, 10));
---
+++ Basics/variables.js
@@ -0,0 +1,28 @@
+let variableCambie = "hola mundo";
+
+const variablesQueNoCambian = "hola mundo no cambia";
+
+//Datos primitivos
+let nombre = "fabian";
+let numeros = 5;
+let booleanos = true;
+let noHayValor = null;
+let noDefinidos = undefined;
+
+
+//symbol
+let simbolo = Symbol("único");
+
+//objetos
+let moto = {
+    marca: "kawazaki",
+    modelo: 1999,
+    valor: 12000000
+};
+
+//arreglo o Array
+let verduras = ["platano", "zapayo", "tomate"];
+
+function nombreDeLaFuncion() {
+
+};
